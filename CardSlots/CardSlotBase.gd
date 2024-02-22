29dde8172f541877d1dc75f373d1ffa5cd5f5315
Santeri <122432859+Peasami@users.noreply.github.com>
2024-02-22 17:49:13 +0200

    CardSlot update

--- CardSlots/CardSlotBase.gd
+++ CardSlots/CardSlotBase.gd
@@ -2,10 +2,3 @@ class_name CardSlotBase
 extends Node2D
 
 @export var card_slot_id: int
-
-func _on_focus_mouse_entered():
-	Events.emit_signal("slot_hovered", true)
-
-func _on_focus_mouse_exited():
-	Events.emit_signal("slot_de_hovered")
-
--- CardSlots/CardSlotFocus.gd
+++ CardSlots/CardSlotFocus.gd
@@ -1,10 +1,7 @@
 extends Node
 
-func _on_focus_mouse_entered():
+func _on_mouse_entered():
 	Events.emit_signal("slot_hovered", true)
 
-func _on_focus_mouse_exited():
+func _on_mouse_exited():
 	Events.emit_signal("slot_de_hovered")
-
-func _on_mouse_entered():
-	pass # Replace with function body.
