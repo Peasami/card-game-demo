0ec94b029d9c91b740ede361f5a3754609c308b1
Santeri <122432859+Peasami@users.noreply.github.com>
2025-08-14 09:53:11 +0300

    getter for cards in hand

--- Autoloads/CardTracking.gd
+++ Autoloads/CardTracking.gd
@@ -13,7 +13,13 @@ func get_cards_in_deck() -> Array[CardBase]:
 		if card.get_state_enum() == GEnums.card_state.IN_DECK:
 			cards_in_deck.append(card)
 	return cards_in_deck
-	
+
+func get_cards_in_hand() -> Array[CardBase]:
+	var cards_in_hand: Array[CardBase]
+	for card: CardBase in cards_manager_node.get_children():
+		if card.get_state_enum() == GEnums.card_state.IN_HAND:
+			cards_in_hand.append(card)
+	return cards_in_hand
 
 var cardsInDeck := []
 var cardsInHand := []
